902e0d46cf719d676c6e5a917e04a2966bf6f0fa
Huxgo <54726985+Huxgo@users.noreply.github.com>
2019-09-01 20:04:24 +0200

    Create 20 transactions
    Could be usefull to make tests

---
+++ spam_transac.py
@@ -0,0 +1,29 @@
+import requests
+import random
+import string
+def randomString(stringLength=10):
+    """Generate a random string of fixed length """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+i=0
+amount = 0
+
+users=[]
+
+while i < 10:
+    users.append(randomString(10))
+    i+=1
+
+i=0
+
+while i < 20:
+    sender = random.randint(0,10)
+    receiver = random.randint(0,10)
+    add = random.randint(1000,150000)
+    amount+=add
+    requests.post('http://localhost/blockchain/transaction.php', data={'from':users[sender], 'to':users[receiver], 'amount':add}).text
+    
+    print(users[sender] + " SEND " + str(add) + " A " + users[receiver])
+    i+=1
+    

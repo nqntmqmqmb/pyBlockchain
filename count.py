fc1d0e9039f15770d54929fcb394822844d6191b
NQNT <54726985+nqntmqmqmb@users.noreply.github.com>
2020-08-06 21:32:52 +0200

    Rename & update
    rename and removing unnused libs

--- count.py
+++ count.py
@@ -1,10 +1,9 @@
-import hashlib
 import time
 import requests
 import json
 
 users = []
-r = requests.get('http://localhost:80/blockchain/block').text
+r = requests.get('http://localhost:80/issoucoin/block').text
 i = 0
 all_blocks = r.split('|||')
 
@@ -13,14 +12,16 @@ while i < len(all_blocks):
     if "{" in all_blocks[i]:
         
         j = json.loads(all_blocks[i])
-        
+    
         if j['data']['transaction'][0]['from'] + ":0" in users:
             pass
+
         else:
             users.append(j['data']['transaction'][0]['from'] + ":0")
             
         if j['data']['transaction'][0]['to'] + ":0" in users:
             pass
+
         else:
             users.append(j['data']['transaction'][0]['to'] + ":0")
             
@@ -37,23 +38,22 @@ while x < len(users):
     while i < len(all_blocks):
     
         if "{" in all_blocks[i]:
-        
             j = json.loads(all_blocks[i])
-    
-            
+
             if users[x].split(':')[0] in j['data']['transaction'][0]['to']:
                 credit = int(users[x].split(':')[1])
                 credit += int(j['data']['transaction'][0]['amount'])
                 users[x] = users[x].split(':')[0] + ":" + str(credit)
                 i+=1
+
             elif users[x].split(':')[0] in j['data']['transaction'][0]['from']:
                 credit = int(users[x].split(':')[1])
                 credit = credit - int(j['data']['transaction'][0]['amount'])
                 users[x] = users[x].split(':')[0] + ":" + str(credit)
                 i+=1
+
             else:
                 i+=1
-            #    users.append(j['data']['transaction'][0]['amount'])
         else:
             i+=1
             x+=1
